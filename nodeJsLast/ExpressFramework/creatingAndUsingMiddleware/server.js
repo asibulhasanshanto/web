8f24d3bb98871227b63d07890f46732c127aac3d
asibulhasanshanto <asibulhasan.rucse.18@gmail.com>
2020-02-27 23:05:52 +0600

    middleware created and used

--- nodeJsLast/ExpressFramework/creatingAndUsingMiddleware/server.js
+++ nodeJsLast/ExpressFramework/creatingAndUsingMiddleware/server.js
@@ -24,15 +24,46 @@ Server bootup or server export
 const express = require('express') 
 const app = express() 
 
-app.use((req,res,next) =>{// this is a middleware
+app.use((req,res,next) =>{// this is a middleware.middlewire is just an object
 	console.log(`${req.method}: ${req.url}`)
 	next()
+})
+app.use((req,res,next) =>{
+	if(req.query.api_key) {//if any key passes when calling then this function works otherwise it stops
+		next()				//procedure of calling with api key=> localhost:3000/accounts?api_key=123 -i
+	}
+	else
+	{
+		res.status(401).send({msg: 'not authorized'})
+	}
 })
 app.get('/', (req, res) => {//these app.get functions are called endpoints
   res.send({msg:'hello world'})
 })
-app.get('/accounts', (req, res) => {//these app.get functions are called endpoints
-  res.send({msg:'hello world'})
+app.get('/accounts', (req,res,next)=>{
+	console.log('accounts in the middlewire')
+	next(new Error('oops'))
+},
+(req, res) => {
+  res.send({msg:'accounts'})
 })
-
+app.get('/transactions', (req, res) => {//these app.get functions are called endpoints
+  res.send({msg:'transactions'})
+})
+app.use((error,req,res,next)=>{//error handaler
+	res.status(500).send(error)
+})
 app.listen(3000)
+//order of iddleware function is important..
+
+/*
+middleware creation
+===================
+const middleware = (request, response, next) => {
+  // Modify request or response
+  // Execute the callback when done
+  next()
+}
+app.use(middleware)
+
+*/
