2622f4ca6f31d6021d1e6c054cb2889f26427551
Asibul <asibulhasan.rucse.18@gmail.com>
2020-04-02 17:52:11 +0600

    node task manager practice

---
+++ Node/task-manager/src/model/user.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+const validator = require("validator")
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim:true,
+        validate(value) {
+            if(value.toLowerCase().includes("password")){
+                throw new Error("password cacnnot be a password")
+            }
+            else if(value.length <=7) {
+                throw new Error("enter at least 7 items")
+            }
+        }
+    },
+    email: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email  is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(val) {
+            if(val< 0) {
+                throw new Error('Invalid age')
+            }
+        }        
+    }
+})
+
+module.exports = User
--- nodeByCoursa/DataDataWhereArtThouData/serverByExpressCommand/app.js
+++ nodeByCoursa/DataDataWhereArtThouData/serverByExpressCommand/app.js
@@ -42,6 +42,7 @@ app.use('/users', usersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+
 app.use('/dishes',dishRouter);
 app.use('/leaders',leaderRouter);
 
