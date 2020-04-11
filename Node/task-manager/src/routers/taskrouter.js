3ddccdaa9560375a670c8c2e60d741f9c462c02d
Asibul <asibulhasan.rucse.18@gmail.com>
2020-04-11 12:36:45 +0600

    data filtering completed

--- Node/task-manager/src/routers/taskrouter.js
+++ Node/task-manager/src/routers/taskrouter.js
@@ -3,10 +3,20 @@ const Task = require('../model/task')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
+
+//GET /tasks?completed=true
 router.get('/tasks',auth,async (req,res) =>{
+    const match = {}
+    
+    if(req.query.completed) {
+        match.completed = req.query.completed === 'true'
+    }
     try {
         //const tasks = await Task.find({ owner: req.user._id})
-        await req.user.populate('tasks').execPopulate()
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send(e)
