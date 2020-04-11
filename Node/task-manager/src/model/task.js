5e7716d8d12d309a5329c315cb961cbb1f0fec62
Asibul <asibulhasan.rucse.18@gmail.com>
2020-04-11 11:26:46 +0600

    timestamps created

--- Node/task-manager/src/model/task.js
+++ Node/task-manager/src/model/task.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const validator = require("validator")
 
-const Task = mongoose.model('Task',{
+const taskSchema = mongoose.Schema({
     description: {
         type: String,
         required:true,
@@ -17,6 +17,9 @@ const Task = mongoose.model('Task',{
         required:true,
         ref: 'User'
     }
+},{
+    timestamps: true
 })
+const Task = mongoose.model('Task',taskSchema)
 
 module.exports = Task
